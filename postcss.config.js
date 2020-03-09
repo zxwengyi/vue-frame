0c5d121712e86fb49b6fbd6859243d348816ae45
zxwengyi <zxwengyi@163.com>
2020-03-09 09:12:30 +0800

    fix: xautoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 
 module.exports = {
   plugins: {
-    'autoprefixer': {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
     'postcss-pxtorem': {
       rootValue: 75, //结果为：设计稿元素尺寸/75，比如元素宽750px,最终页面会换算成 10rem
       propList: ['*']
