d39b065ecf93b290205fec11b0507236e9c76b41
dfs831 <dfs831@365gl.com>
2020-03-26 15:56:22 +0800

    fit:代码分割

--- vue.config.js
+++ vue.config.js
@@ -27,24 +27,26 @@ module.exports = {
   },
 
   chainWebpack: config => {
-    if (IS_DEV) { // 开发环境
-      config.plugin('html').tap(args => {
-        args[0].isProd = false
-        return args
-      })
-    } else { // 生产环境
-      config.plugin('html').tap(args => {
-        args[0].isProd = true
-        return args
-      })
-      // 使用cdn加载插件
-      config.set("externals", {
-        vue: "vue",
-        axios: "axios",
-        "vue-router": "VueRouter"
-      }),
-        // 移除 prefetch 插件
-        config.plugins.delete("prefetch");
+    if (IS_DEV) {
+      // 开发环境
+      config.plugin("html").tap(args => {
+        args[0].isProd = false;
+        return args;
+      });
+    } else {
+      // 生产环境
+      config.plugin("html").tap(args => {
+        args[0].isProd = true;
+        return args;
+      });
+      // // 使用cdn加载插件
+      // config.set("externals", {
+      //   vue: "vue",
+      //   axios: "axios",
+      //   "vue-router": "VueRouter"
+      // }),
+      // 移除 prefetch 插件
+      config.plugins.delete("prefetch");
       // 移除 preload 插件
       config.plugins.delete("preload");
       // 压缩代码
@@ -53,23 +55,45 @@ module.exports = {
       config.optimization.splitChunks({
         chunks: "all",
         cacheGroups: {
-          libs: {
-            name: "chunk-libs",
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10,
-            chunks: "initial" // only package third parties that are initially dependent
+          vue_chunk: {
+            name: "vue_chunk",
+            test: /[\\/]node_modules[\\/]vue[\\/]/,
+            chunks: "all",
+            priority: 50,
+            reuseExistingChunk: true,
+            enforce: true
           },
-          vantUI: {
-            name: "chunk-vantUI", // split vantUI into a single package
-            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-            test: /[\\/]node_modules[\\/]_?vant(.*)/ // in order to adapt to cnpm
+          vuex_chunk: {
+            name: "vuex_chunk",
+            test: /[\\/]node_modules[\\/]_?vuex[\\/]/,
+            chunks: "all",
+            priority: 40,
+            reuseExistingChunk: true,
+            enforce: true
+          },
+          axios_chunk: {
+            name: "axios_chunk",
+            test: /[\\/]node_modules[\\/]_?axios[\\/]/,
+            chunks: "all",
+            priority: 30,
+            reuseExistingChunk: true,
+            enforce: true
+          },
+          vue_router_chunk: {
+            name: "vue_router_chunk",
+            test: /[\\/]node_modules[\\/]_?vue-router[\\/]/,
+            chunks: "all",
+            priority: 20,
+            reuseExistingChunk: true,
+            enforce: true
           },
           commons: {
             name: "chunk-commons",
-            test: resolve("src/components"), // can customize your rules
-            minChunks: 3, //  minimum common number
-            priority: 5,
-            reuseExistingChunk: true
+            test: /[\\/]node_modules[\\/]/,
+            chunks: "all",
+            priority: 10,
+            reuseExistingChunk: true,
+            enforce: true
           }
         }
       });
